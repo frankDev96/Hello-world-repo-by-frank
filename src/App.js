f4292553166b48d7c3bef468c1b66889f0fe9d49
Franklin <franklinsamuvel955@gmail.com>
2024-09-09 20:49:10 +0530

    text update

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header" >
         <p>
           <h1 className='textStyle'>Hello World</h1> by <h4>Franklin Samuvel</h4>
+          <h4>Software developer</h4>
         </p>
       </header>
     </div>
